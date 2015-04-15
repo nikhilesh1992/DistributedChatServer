5637a4e953e646e936fea1bb3ffdcdd64de82955
Samarth Shah <ssamarth@seas.upenn.edu>
2015-04-15 15:12:16 -0400

    added includes and defines Signed-off-by: Samarth Shah <ssamarth@seas.upenn.edu>

--- dchat.c
+++ dchat.c
@@ -68,3 +68,78 @@
 			controllerLeader(buf, arrayString);
 			
 		}
+		
+		else if(amILeader(nameOfUser) == 0)
+		{
+			if ( recvlen=recvfrom(socketIdentifier, buf, sizeof(buf),0,(SA *)&incomingAddr,&len) < 0)	// The 0,0 can be used to check if the leader IP address has changed over the time
+			{        
+				perror( "Error in recvfrom on Server socketIdentifier");
+				exit(1);
+			}
+			strcpy(tempbuf,buf);
+			generalisedStringTok(tempbuf,arrayS);
+			if(strcmp(buf,"Table") == 0)
+			{
+				if ( recvlen=recvfrom(socketIdentifier, bufRecvdTable, sizeof(bufRecvdTable), 0,0,0) < 0)	// The 0,0 can be used to check if the leader IP address has changed over the time
+				{        
+					perror( "Error in recvfrom on Server socketIdentifier");
+					exit(1);
+				}
+				else
+				{
+					//acknowledgement in the form of table received, reset timer1
+					timer1 = 0;
+					copyUserDatabaseTable(bufRecvdTable);
+					updateLeaderAddress();
+					updateTimeStamper();
+					resetAllSeqNum();
+					controllerAcknowledgement("AckTable", nameOfUser);
+				}
+			}
+			else if(strcmp(buf,"String") == 0)
+			{
+				//receive message with timestamper
+				if ( recvlen = recvfrom(socketIdentifier, buf, sizeof(buf), 0,0,0) < 0)	// The 0,0 can be used to check if the leader IP address has changed over the time
+				{        
+					perror( "Error in recvfrom on Server socketIdentifier");
+					exit(1);
+				}
+				controllerNonLeader(buf, arrayString2);
+			}
+			else if(strcmp(arrayS[0].String,"Add") == 0)
+			{
+				//receive message with timestamper
+				char tempPort[10];
+				strcpy(tempbuf,"Alert");
+				strcat(tempbuf,"~");
+				strcat(tempbuf, arrayS[1].String);
+				strcat(tempbuf,"~");
+				strcat(tempbuf,inet_ntoa(incomingAddr.sin_addr));
+				strcat(tempbuf,"~");
+				sprintf(tempPort,"%d",ntohs(incomingAddr.sin_port));
+				strcat(tempbuf,tempPort);
+				if (sendto(socketIdentifier,tempbuf, sizeof(tempbuf), 0,(SA *)&leaderaddr, sizeof(leaderaddr)) < 0)	
+				{
+					perror("Error in sendto from server to client");
+					return 0;
+				}
+			}
+
+		}
+	}
+
+	pthread_attr_destroy(&attribute);	// Joining the internal threads with the main thread so that the main terminates only after the internal threads have completed
+	for(i=0; i<THREADNUMBER; i++) 
+	{
+	    createReturn = pthread_join(threadMessaging[i], &state);
+	    if (createReturn) 
+	    {
+		    fprintf(stderr,"In joining threads pthread_join() is %d\n", createReturn);
+		    exit(-1);
+	    }
+    }
+
+    sem_destroy(&S1);
+
+	return 0;
+}
--- dchat.h
+++ dchat.h
@@ -1,3 +1,34 @@
+//Header files
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include <time.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+#define SA struct sockaddr
+
+//Error codes and exit status
+#define ERR_SOCKET 1
+#define ERR_BIND 2
+#define ERR_LISTEN 3
+#define ERR_ACCEPT 4
+#define BUFSIZE 500
+#define THREADNUMBER 8
+
+//Structure definitions
+
 struct chatUserInfo 
 {
 	int ID;
